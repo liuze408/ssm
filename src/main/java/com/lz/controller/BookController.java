13dd22b8d602a13682691643bb0047014df2efcf
liuze <liuze1110@163.com>
2022-04-01 16:48:28 +0800

    tushu commit

---
+++ src/main/java/com/lz/controller/BookController.java
@@ -0,0 +1,77 @@
+package com.lz.controller;
+
+import com.lz.pojo.Books;
+import com.lz.service.BookService;
+import com.sun.org.apache.xpath.internal.operations.Mod;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.awt.print.Book;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/book")
+public class BookController {
+
+    @Autowired
+    @Qualifier("BookServiceImpl")
+    private BookService bookService;
+
+    @RequestMapping("/allBook")
+    public String list(Model model) {
+        List<Books> list = bookService.queryAllBook();
+        model.addAttribute("list", list);
+        return "allBook";
+    }
+
+    @RequestMapping("/toAddBook")
+    public String toAddPager(){
+        return "addBook";
+    }
+
+    @RequestMapping("/addBook")
+    public String addPaper(Books books) {
+        System.out.println(books);
+        bookService.addBook(books);
+        return "redirect:/book/allBook";
+    }
+
+
+    @RequestMapping("/toUpdateBook")
+    public String toUpdateBook(Model model, int id) {
+        Books books = bookService.queryBookById(id);
+        System.out.println(books);
+        model.addAttribute("book",books );
+        return "updateBook";
+    }
+
+    @RequestMapping("/updateBook")
+    public String updateBook(Books books){
+        bookService.updateBook(books);
+        return "redirect:/book/allBook";
+    }
+
+    @RequestMapping("/deleteBook")
+    public String deleteBook(int id){
+        bookService.deleteBookById(id);
+        return "redirect:/book/allBook";
+    }
+
+    @RequestMapping("/queryBook")
+    public String queryBook(String queryBookName,Model model){
+        Books books=bookService.queryBookByName(queryBookName);
+        List<Books> list=new ArrayList<Books>();
+        list.add(books);
+        if(books==null){
+            list=bookService.queryAllBook();
+            model.addAttribute("error","未查到");
+        }
+        model.addAttribute("list",list);
+        return "allBook";
+    }
+
+}
